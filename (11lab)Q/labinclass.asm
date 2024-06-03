f90e11d16c6c55d5900b4b342607b46990de6ee9
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-06-03 21:19:52 +1000

    Update labinclass.asm

--- (11lab)Q/labinclass.asm
+++ (11lab)Q/labinclass.asm
@@ -110,3 +110,55 @@ print_digits:
 
     popa                  ; Restore all registers
     ret
+
+
+
+
+
+
+section .data
+    output_format db "%d ", 0
+
+section .text
+    global _start
+
+_start:
+    mov ecx, 10000       ; количество чисел, которые нужно вычислить
+    mov esi, 1           ; t[n-1]
+    mov edi, 1           ; t[n-2]
+    mov ebx, 1           ; t[n-3]
+    mov edx, 9           ; количество чисел, которые нужно вывести
+
+calculate_next:
+    ; Считаем следующее число по алгоритму
+    mov eax, esi
+    sub eax, edi
+    mov ecx, [ebx + eax*4]
+    add esi, ecx
+
+    mov eax, edi
+    sub eax, ebx
+    mov ecx, [esi + eax*4]
+    add edi, ecx
+
+    mov eax, ebx
+    sub eax, esi
+    mov ecx, [edi + eax*4]
+    add ebx, ecx
+
+    ; Выводим число на консоль
+    mov eax, esi
+    mov [esp], eax
+    add esp, 4
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, output_format
+    int 0x80
+
+    dec edx
+    jnz calculate_next
+
+exit:
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
