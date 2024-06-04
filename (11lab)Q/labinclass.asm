7a13cbda363bd6d9735d9b541b45cde5c490a816
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-06-04 22:46:17 +1000

    Update labinclass.asm

--- (11lab)Q/labinclass.asm
+++ (11lab)Q/labinclass.asm
@@ -2,6 +2,35 @@ fiboo.o: warning: relocation in read-only section `.text'
 /usr/bin/ld: warning: creating DT_TEXTREL in a PIE
 
 
+mov ecx, 3                 ; start with a[4]
+calc_loop:
+    cmp ecx, n
+    jg print_last_10
+
+    ; Calculate a[n] = a[n - a[n-1]] + a[n-1 - a[n-2]] + a[n-2 - a[n-3]]
+    mov eax, ecx
+    sub eax, 1
+    mov ebx, [array + eax*4]   ; a[n-1]
+    sub eax, ebx
+    mov edx, eax               ; save a[n - a[n-1]]
+
+    mov eax, ecx
+    sub eax, 2
+    mov ebx, [array + eax*4]   ; a[n-2]
+    sub eax, ebx
+    add edx, [array + eax*4]   ; + a[n-1 - a[n-2]]
+
+    mov eax, ecx
+    sub eax, 3
+    mov ebx, [array + eax*4]   ; a[n-3]
+    sub eax, ebx
+    add edx, [array + eax*4]   ; + a[n-2 - a[n-3]]
+
+    mov [array + ecx*4], edx   ; store the result
+    inc ecx
+    jmp calc_loop
+
+
 
 section .data
     n equ 10000
